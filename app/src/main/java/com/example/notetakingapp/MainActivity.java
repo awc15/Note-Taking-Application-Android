0bd0fbd50a7ca667a556e56aa7286be45fba486b
Abdul Wahab Ch <abdul.wahab@coeus-solutions.de>
2020-09-30 11:29:48 +0500

    First Activity

--- app/src/main/java/com/example/notetakingapp/MainActivity.java
+++ app/src/main/java/com/example/notetakingapp/MainActivity.java
@@ -1,14 +1,45 @@
 package com.example.notetakingapp;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    FloatingActionButton fab;
+    AlertDialog.Builder builder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fab = findViewById(R.id.floatingButton);
+    }
+
+    public void AddNote(View view) {
+        startActivity(new Intent(getApplicationContext(), AddNote.class));
+
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        new AlertDialog.Builder(this)
+                .setTitle("Exit Application")
+                .setMessage("Are you sure you want to exit?")
+                .setNegativeButton(android.R.string.no, null)
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+
+                    public void onClick(DialogInterface arg0, int arg1) {
+                        MainActivity.super.onBackPressed();
+                    }
+                }).create().show();
     }
 }
