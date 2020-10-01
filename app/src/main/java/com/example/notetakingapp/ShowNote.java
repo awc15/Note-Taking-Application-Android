90f65383522e1d3a04772ef985d81ea2693d9b35
Abdul Wahab Ch <abdul.wahab@coeus-solutions.de>
2020-10-01 11:08:41 +0500

    Add note and display notes completed working on update and delete

--- app/src/main/java/com/example/notetakingapp/ShowNote.java
+++ app/src/main/java/com/example/notetakingapp/ShowNote.java
@@ -1,34 +1,47 @@
 package com.example.notetakingapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.Toolbar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import static android.os.Build.ID;
 
 public class ShowNote extends AppCompatActivity {
 
-    TextView title, detail, date, time;
-    SQLiteDatabase mDatabase;
-    NotesDBHelper dbHelper;
-
+    private TextView title, detail, date, time;
+    private SQLiteDatabase mDatabase;
+    private NotesDBHelper dbHelper;
+    private Button updDelButton;
+    private long ID;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_note);
-        long ID = getIntent().getIntExtra("ID", 0);
-        String SEARCH_QUERY = "SELECT * FROM " + NotesContract.Notes.TABLE_NAME +
-                " WHERE " + NotesContract.Notes._ID + " = " + ID;
+        ID = getIntent().getIntExtra("ID", 0);
         // Toast.makeText(getApplicationContext(),"ID : "+ ID,Toast.LENGTH_SHORT).show();
+        getSupportActionBar().hide();
         title = findViewById(R.id.title);
         detail = findViewById(R.id.detail);
         date = findViewById(R.id.date);
         time = findViewById(R.id.time);
         dbHelper = new NotesDBHelper(this);
         mDatabase = dbHelper.getReadableDatabase();
+        showData();
 
+
+
+
+
+
+
+
+        }
+
+    private void showData() {
         Cursor cursor = mDatabase.query(NotesContract.Notes.TABLE_NAME,
                 new String[]{NotesContract.Notes.NOTES_TITLE, NotesContract.Notes.NOTES_DESCRIPTION,
                         NotesContract.Notes.NOTES_TIME,NotesContract.Notes.NOTES_DATE},
@@ -37,22 +50,12 @@ public class ShowNote extends AppCompatActivity {
 
         if (cursor != null)
             cursor.moveToFirst();
-       // String checkTitle = ;
+        // String checkTitle = ;
         title.setText(cursor.getString(cursor.getColumnIndex(NotesContract.Notes.NOTES_TITLE)));
         detail.setText(cursor.getString(cursor.getColumnIndex(NotesContract.Notes.NOTES_DESCRIPTION)));
         date.setText(cursor.getString(cursor.getColumnIndex(NotesContract.Notes.NOTES_DATE)));
         time.setText(cursor.getString(cursor.getColumnIndex(NotesContract.Notes.NOTES_TIME)));
         cursor.close();
-
-
-
-
-
-
-//        Cursor cursor = mDatabase.rawQuery(SEARCH_QUERY, null);
-//        if (cursor != null) {
-//            cursor.moveToNext();
-//            title.setText(cursor.getColumnIndex(NotesContract.Notes.NOTES_TITLE));
-        }
-
     }
+
+}
